0b6b2680d3c01fd55a67bff90383494b33cfaba4
P2541536 <119421978+P2541536@users.noreply.github.com>
2023-02-23 19:04:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
-
+var randomNum;
+var input;
+function randomNumGenerator(){
+	randomNum =Math.floor(Math.random()*41)-20;
+	document.getElementById("Num").innerHTML = randomNum;
+	GuessName();
+	print();
+}
+Function gessName(){
+	input = parseInt(document.getElementById("gess").value);
+	
+}
+function print(){
+	if(Math.abs(randomNum-input)<=5){
+		document.getElementById("respond").innerHTML ="Hot";
+		
+	}
+	else{
+		document.getElementById("respond").innerHTML ="Cold";
+	}
+}
+	
+}
